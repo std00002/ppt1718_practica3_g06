c70e302638bb27b5963f993e5d027c59fdca98d6
Salvador <std00002@red.ujaen.es>
2017-12-17 12:27:28 +0100

    Soporte a codigos de estado.

--- src/ujaen/git/ppt/p3/HTTPSocketConnection.java
+++ src/ujaen/git/ppt/p3/HTTPSocketConnection.java
@@ -2,6 +2,7 @@ package ujaen.git.ppt.p3;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,8 +19,6 @@ import java.util.Random;
  */
 public class HTTPSocketConnection implements Runnable {
     
-    public static final String HTTP_Ok="200";
-    
     private Socket mSocket=null;
     
     /**
@@ -29,7 +28,9 @@ public class HTTPSocketConnection implements Runnable {
     public HTTPSocketConnection(Socket s){
         mSocket = s;
     }
+    
     public void run() {
+        //Variables
         Random r = new Random(System.currentTimeMillis());
         int n=r.nextInt();
         String request_line="";
@@ -37,33 +38,63 @@ public class HTTPSocketConnection implements Runnable {
         DataOutputStream output;
         FileInputStream input_file;
         try {
+            byte[] outdata=null;
             String outmesg="";
             input = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
             output = new DataOutputStream(mSocket.getOutputStream());
             do{
-                
+               
                 request_line= input.readLine();
-                //Esto se debería sacar de esta bucle
-                if(request_line.startsWith("GET "))
-                {
-                    String parts[]=request_line.split(" ");
-                    if(parts.length==3)
-                    {
-                        if(parts[1].equalsIgnoreCase("/")){
-                         outmesg="HTTP/1.1 200 OK\r\nContent-type:text/html\r\n\r\n<html><body><h1>HOLA"+n+" veces</h1></body></html>";
-                        }else{
-                        outmesg="HTTP/1.1 404\r\nContent-type:text/html\r\n\r\n<html><body><h1>No encontrado</h1></body></html>";}
-                            
-                    }
-                    else{
-                        outmesg="HTTP/1.1 400\r\n";
-                    }
+                //Esto se deberÃ­a sacar de este bucle
+                String parts[]=request_line.split(" ");
+                if(request_line.startsWith("GET ")){
+                    String resourceFile="";
+               
+                    
+                    if(parts.length==3){
+                        //Version soportada
+                        String[] status_line;
+                        status_line=parts[2].split("/");
                         
-                
+                        if(status_line[1].equals("1.1") || status_line[1].equals("1.0")){
+                            
+                        if(!parts[0].equals("GET ")){
+                            outmesg="HTTP/1.1 405\r\nContent-type:text/html\r\n\r\n<html><body><h1>Metodo no permitido</h1></body></html>";
+                       outdata=outmesg.getBytes();    
+                        }
+                        if(parts[1].equalsIgnoreCase("/")){
+                            resourceFile="index.html";
+                        }else
+                            resourceFile=parts[1];
+                        //Content-type
+                          
+                        outdata=leerRecurso(resourceFile);
+                        //Cabecera content-length
+                        if(outdata==null)    {
+                            outmesg="HTTP/1.1 404\r\nContent-type:text/html\r\n\r\n<html><body><h1>No encontrado</h1></body></html>";
+                            outdata=outmesg.getBytes();    
+                        }
+                   // }
+                    
+                    else{
+                        outmesg="HTTP/1.1 400\r\nContent-type:text/html\r\n\r\n<html><body><h1>Error de sintaxis/cliente</h1></body></html>";
+                        outdata=outmesg.getBytes();
+                    }
+                    }else{
+                        outmesg="HTTP/1.1 505\r\nContent-type:text/html\r\n\r\n<html><body><h1>HTTP Version Not Supported</h1></body></html>";
+                        outdata=outmesg.getBytes();
+                        }  
+            } 
+                    
                 }
+                
+                
                 System.out.println(request_line);
             }while(request_line.compareTo("")!=0);
-           output.write(outmesg.getBytes());
+            //CABECERAS....
+            
+            //Recurso
+           output.write(outdata);
             input.close();
             output.close();
             mSocket.close();
@@ -72,4 +103,16 @@ public class HTTPSocketConnection implements Runnable {
             System.err.println("Exception" + e.getMessage());
         }
         }
+
+    /**
+     * MÃ©todo para leer un recurso del disco
+     * @param resourceFile
+     * @return los bytes del archio o null si Ã©ste no existe
+     */
+    private byte[] leerRecurso(String resourceFile) {
+        //Se debe comprobar que existe
+        File f;
+        
+        return null;
+    }
 }
